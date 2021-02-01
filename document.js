a1a6d3b43b1c1cb58edcccad96ef86bb116849b6
Quentin Lurkin <qlurkin@gmail.com>
2021-02-01 23:54:46 +0100

    Update Document

--- document.js
+++ document.js
@@ -5,16 +5,24 @@
 	  const script = document.createElement('script');
 	  script.async = true;
 
-	  script.onload = () => {
+	  const callback = () => {
 	    resolve();
 	  };
 
-	  script.onerror = () => {
-	    reject();
-	  };
+	  script.onload = callback;
+	  script.onreadystatechange = callback;
+	  script.setAttribute("src", url); //document.head.insertBefore(script , null)
 
-	  script.setAttribute("src", url);
-	  document.head.insertBefore(script, null);
+	  document.body.appendChild(script); // const script = document.createElement('script')
+	  // script.async = true
+	  // script.onload = () => {
+	  // 	resolve()
+	  // }
+	  // script.onerror = () => {
+	  // 	reject()
+	  // }
+	  // script.setAttribute("src", url)
+	  // document.head.insertBefore(script , null)
 	});
 	const loadCSS = url => new Promise(resolve => {
 	  const link = document.createElement('link');
@@ -28,45 +36,6 @@
 	  document.head.insertBefore(link, null);
 	});
 
-	const getLanguage = element => {
-	  let language = null;
-	  element.classList.forEach(className => {
-	    const re = /(lang-|language-)(\w+)/;
-	    language = className.replace(re, '$2');
-	  });
-	  return language;
-	};
-	const findCode = document => {
-	  return document.querySelectorAll('code[class*=lang]');
-	};
-	const findLanguages = document => {
-	  const languages = [];
-	  findCode(document).forEach(element => {
-	    const language = getLanguage(element);
-
-	    if (language) {
-	      languages.push(language);
-	    }
-	  });
-	  return [...new Set(languages)];
-	};
-
-	const loadPrism = () => {
-	  const prismCdnUrl = language => `https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-${language}.js`;
-
-	  loadCSS('https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism.min.css');
-	  loadScript('https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/prism.min.js').then(() => loadScript('https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/plugins/normalize-whitespace/prism-normalize-whitespace.min.js')).then(() => {
-	    const languages = findLanguages(document);
-	    console.log('languages:', languages);
-	    Promise.all(languages.map(language => loadScript(prismCdnUrl(language)).catch(() => {
-	      console.log(`${language} not found`);
-	    }))).then(() => {
-	      console.log("languages loaded");
-	      Prism.highlightAll();
-	    });
-	  });
-	};
-
 	const loadMathjax = () => {
 	  window.MathJax = {
 	    loader: {
@@ -76,6 +45,14 @@
 	    svg: {}
 	  };
 	  loadScript('https://polyfill.io/v3/polyfill.min.js?features=es6').then(() => loadScript('https://cdn.jsdelivr.net/npm/mathjax@3.0.0/es5/startup.js'));
+	};
+
+	const loadPrism = () => {
+	  console.log('Load Prism.js');
+	  loadCSS('https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css');
+	  loadScript('https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-core.min.js').then(() => loadScript('https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js')).then(() => loadScript('https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/autoloader/prism-autoloader.min.js')).then(() => {
+	    Prism.highlightAll();
+	  });
 	};
 
 	window.addEventListener("DOMContentLoaded", event => {
