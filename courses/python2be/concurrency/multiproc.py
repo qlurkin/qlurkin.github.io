748acf3c6515d8b264c64cb34fe7a4f355e92ca5
Quentin Lurkin <qlurkin@gmail.com>
2021-05-03 10:01:42 +0200

    Examples

--- courses/python2be/concurrency/multiproc.py
+++ courses/python2be/concurrency/multiproc.py
@@ -4,5 +4,5 @@ def compute(data):
 	return data ** 2
 
 if __name__ == '__main__':
-	with mp.Pool(3) as pool:
-		print(pool.map(compute, [1, 7, 8, -2]))
+	with mp.Pool(10) as pool:
+		print(pool.map(compute, [1, 7, 8, -2, 1, 7, 8, -2, 1, 7, 8, -2, 1, 7, 8, -2]))
--- courses/python2be/concurrency/multithr.py
+++ courses/python2be/concurrency/multithr.py
@@ -1,7 +1,8 @@
 import concurrent.futures as cf
+import os
 
 def compute(data):
 	return data ** 2
 
-with cf.ThreadPoolExecutor(3) as executor :
+with cf.ThreadPoolExecutor(os.cpu_count()) as executor :
 	print(list(executor.map(compute, [1, 7, 8, -2])))
