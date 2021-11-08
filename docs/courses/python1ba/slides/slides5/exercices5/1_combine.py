f719384cde0acab510aaf34e0cdf3ac30b9dfeb4
Quentin Lurkin <qlurkin@gmail.com>
2021-11-08 10:05:25 +0100

    Course 5 1BA

---
+++ docs/courses/python1ba/slides/slides5/exercices5/1_combine.py
@@ -0,0 +1,13 @@
+# Ecrire une fonction nommée combine() qui prends deux listes et une fonction
+# en paramètres. Les deux listes seront de la même taille et la fonction reçue
+# devra prendre deux paramètres.
+# La fonction combine() appeler la fonction reçue avec une valeur de chaque
+# liste dans l'ordre. Elle renverra la liste contenant les valeurs de retour.
+
+def combine(L1, L2, fct):
+    # Votre code ici
+
+def add(a, b):
+    return a + b
+
+print(combine([1, 2, 3], [3, 2, 1], add)) # [4, 4, 4]
