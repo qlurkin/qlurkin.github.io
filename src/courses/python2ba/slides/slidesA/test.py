a657a314123a705ea7f86de20d128bde0df3f51e
Quentin Lurkin <qlurkin@gmail.com>
2021-12-21 08:31:55 +0100

    Add things

--- src/courses/python2ba/slides/slidesA/test.py
+++ src/courses/python2ba/slides/slidesA/test.py
@@ -26,3 +26,19 @@ plt.plot(x, y)
 plt.plot(x, dydx)
 plt.plot(x, y_int)
 plt.show()
+
+x = np.linspace(-2*np.pi, 2*np.pi, 100)
+
+plt.plot(x, np.cos(x), linewidth=2.5 , label='cos(x)')
+plt.plot(x, np.sin(x), color='red', label='sin(x)')
+
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.ylim(-2, 2)
+plt.xticks(
+    [-2*np.pi, -np.pi, 0, np.pi, 2*np.pi],
+    [r'$-2\pi$', r'$\pi$', '0', r'$\pi$', r'$2\pi$']
+)
+plt.legend(loc='upper right')
+plt.show()
