a18605eb6606288830e02cd62da12b213e974f60
Quentin Lurkin <qlurkin@gmail.com>
2022-02-23 13:02:47 +0100

    Cleanup

--- src/courses/python2be/concurrency/multiproc.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing as mp
-
-def compute(data):
-	return data ** 2
-
-if __name__ == '__main__':
-	with mp.Pool(10) as pool:
-		print(pool.map(compute, [1, 7, 8, -2, 1, 7, 8, -2, 1, 7, 8, -2, 1, 7, 8, -2]))
