11386c4b6c218c2a9ca95413e47e05f44bab5b3d
Quentin Lurkin <qlurkin@gmail.com>
2022-03-31 10:23:54 +0200

    Rust Notes

--- src/courses/.rust/basic/src/main.rs
+++ src/courses/.rust/basic/src/main.rs
@@ -508,5 +508,57 @@ fn main() {
 
     // No problem to have multiple impl block for the same type
 
-    
+    // Enum
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    enum IpAddr2 {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr2::V4(127, 0, 0, 1);
+    let loopback = IpAddr2::V6(String::from("::1"));
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    // Option Enum
+    enum Option<T> {
+        None,
+        Some(T),
+    }
+
+    // So used that we don't have to import it. We can also use None and Some without mentioning Option<T>::
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+
+
 }
