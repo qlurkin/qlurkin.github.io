4ba60b744fac038bab8c12782a932a26fd5ca67f
Quentin Lurkin <qlurkin@gmail.com>
2023-06-09 08:48:12 +0200

    Delete Archive

--- archive/template.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-
-env = Environment(
-    loader=FileSystemLoader("templates"),
-    autoescape=select_autoescape()
-)
-
-def load(name):
-    return env.get_template(name+'.html')
