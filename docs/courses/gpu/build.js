013b8be5587a33f712addf57746b3ba202b718e8
Quentin Lurkin <qlurkin@gmail.com>
2023-09-16 09:54:10 +0200

    Ready for begining of the year

--- docs/courses/gpu/build.js
+++ docs/courses/gpu/build.js
@@ -1,9 +1,10 @@
 import { build_and_index } from "build_lib";
 
 build_and_index([
-  'introgpu',
+  // 'introrust',
+//  'introgpu',
   'labo1',
-  'labo3',
-  'labo4',
-  'cloth'
+//  'labo3',
+//  'labo4',
+//  'cloth'
 ], {title: 'GPU Computing'})
