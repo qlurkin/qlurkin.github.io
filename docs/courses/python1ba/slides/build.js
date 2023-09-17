39feb2dbeaa9af92c9e01488dda1048faeecd820
Quentin Lurkin <qlurkin@gmail.com>
2023-09-17 18:18:48 +0200

    Reset Python 1BA

--- docs/courses/python1ba/slides/build.js
+++ docs/courses/python1ba/slides/build.js
@@ -2,8 +2,8 @@ import { build_and_index } from "build_lib";
 
 build_and_index([
   'slides1',
-  'slides2',
-  'slides3',
-  'slides4',
-  'slides5',
+//  'slides2',
+//  'slides3',
+//  'slides4',
+//  'slides5',
 ], {title: 'Python Part 1'})
