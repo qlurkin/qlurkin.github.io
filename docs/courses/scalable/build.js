a7a353e64294be3982aba38c56c6b5422ca6db8e
Quentin Lurkin <qlurkin@gmail.com>
2023-09-19 14:55:59 +0200

    Add Scalable lab 1

--- docs/courses/scalable/build.js
+++ docs/courses/scalable/build.js
@@ -1,6 +1,7 @@
 import { build_and_index } from 'build_lib'
 
 build_and_index([
+  'docker',
   // 'intro',
   // 'mandelbrot',
 ], {title: 'Scalable Architecture'})
---
+++ docs/courses/scalable/docker/build.js
@@ -0,0 +1,3 @@
+import { redirect } from 'build_lib'
+
+redirect('https://docs.docker.com/get-started/', {title: 'Labo 1: Getting Started with Docker'})
