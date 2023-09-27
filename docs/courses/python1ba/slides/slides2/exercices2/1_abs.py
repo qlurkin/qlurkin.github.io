e27ea6fa91768e526591ce9742abba0282daedef
Quentin Lurkin <qlurkin@gmail.com>
2023-09-27 22:58:12 +0200

    1BA Deck 2

--- docs/courses/python1ba/slides/slides2/exercices2/1_abs.py
+++
@@ -1,9 +0,0 @@
-# Ecrire une fonction nommée abs() qui prend un nombre en
-# paramètre et qui renvoie la valeur absolue de ce nombre.
-
-def abs(x):
-	# Votre code ici
-
-if __name__ == '__main__':
-	print(abs(5))
-	print(abs(-3))
--- docs/courses/python1ba/slides/slides2/exercices2/3_max.py
+++
@@ -1,8 +0,0 @@
-# Ecrire une fonction nommée max() qui reçoit
-# 3 nombres et renvoie le plus grand des trois.
-
-def max(a, b, c):
-	# Votre code ici
-
-if __name__ == '__main__':
-	print(max(3, 42, -5))
