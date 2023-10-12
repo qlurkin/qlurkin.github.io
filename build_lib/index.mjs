1aac7e2b2a15f502a977b112faba83f59452195f
Quentin Lurkin <qlurkin@gmail.com>
2023-10-12 13:39:08 +0200

    add typst

--- build_lib/index.mjs
+++ build_lib/index.mjs
@@ -7,12 +7,88 @@ import yaml from 'js-yaml'
 import puppeteer from 'puppeteer'
 import express from 'express'
 import { PDFDocument } from 'pdf-lib'
+import compile from './typst.mjs'
 
 const build_script = 'build.js'
 
+function isString(obj) {
+  return typeof obj === 'string' || obj instanceof String
+}
+
+export function update(props, key_or_obj, value) {
+  const res = Object.assign({}, props)
+  if (isString(key_or_obj)) {
+    const key = key_or_obj
+    res[key] = value
+  } else {
+    const obj = key_or_obj
+    Object.assign(res, obj)
+  }
+  return res
+}
+
+export async function read(filename) {
+  let props = update({}, 'filename', filename)
+  const info = parse(filename)
+  props = update(props, 'name', info.name)
+  props = update(props, 'ext', info.ext)
+  props = update(props, 'dir', info.dir)
+  props = update(
+    props,
+    'content',
+    await readFile(filename, { encoding: 'utf8' }),
+  )
+  return props
+}
+
+export function prelude(props) {
+  if (props.content.startsWith('---\n')) {
+    const parts = props.content.slice(4).split('\n---\n')
+    const prelude = parts[0]
+    props = update(props, 'content', parts.slice(1).join('\n---\n').trimStart())
+    props = update(props, yaml.load(prelude))
+  }
+  return props
+}
+
+export async function md(props) {
+  props = update(
+    props,
+    'content',
+    await marked.parse(props.content, { async: true }),
+  )
+  props = update(props, 'dest', `${props.name}.html`)
+  return props
+}
+
+export function template(props, layout) {
+  if (layout) {
+    props = update(props, 'layout', layout)
+  }
+  props = update(
+    props,
+    'content',
+    nunjucks.render(`${props.layout}.njk`, props),
+  )
+  return props
+}
+
+export async function write(props, dest) {
+  if (dest) {
+    props = update(props, 'dest', dest)
+  }
+  await writeFile(props.dest, props.content)
+  return props
+}
+
+export async function typst(props, font_size) {
+  props = update(props, 'content', await compile(props.content, font_size))
+  return props
+}
+
 async function get_parent_dir_with(filename) {
   let current = '.'
-  while(!(await readdir(current)).includes(filename)) {
+  while (!(await readdir(current)).includes(filename)) {
     current = join(current, '..')
   }
   return resolve(current)
@@ -23,14 +99,15 @@ async function get_root() {
 }
 
 function enforceList(obj) {
-    if(!obj) return
-    if(!Array.isArray(obj))
-        return [obj]
-    return obj
+  if (!obj) return
+  if (!Array.isArray(obj)) return [obj]
+  return obj
 }
 
 const root = await get_root()
-const server = JSON.parse(await readFile(join(root, 'server.json'), {encoding: 'utf8'}))
+const server = JSON.parse(
+  await readFile(join(root, 'server.json'), { encoding: 'utf8' }),
+)
 const server_root = join(root, server.root)
 
 const env = nunjucks.configure(join(root, 'layouts'), { autoescape: true })
@@ -54,46 +131,43 @@ function runScript(scriptPath, args, options) {
   return new Promise((resolve, reject) => {
     const process = child_process.fork(scriptPath, args, options)
 
-    process.on('error', function (error) {
+    process.on('error', function(error) {
       const err = new Error(`Error in '${scriptPath}': ${error}`)
       reject(err)
     })
 
-    process.on('exit', function (code) {
+    process.on('exit', function(code) {
       resolve(code)
     })
   })
 }
 
-async function build_dir(path) {
-  if(path === '.') return
+export async function dir(path) {
+  if (path === '.') return
   const files = await readdir(path)
-  if(files.includes(build_script)) {
+  if (files.includes(build_script)) {
     await runScript(build_script, [], {
-      cwd: path
+      cwd: path,
     })
   }
 }
 
-async function build_md(path) {
-  const info = parse(path)
-  let content = await readFile(path, {encoding: 'utf8'})
-  const opts = {layout: 'document'}
-  if(content.startsWith('---\n')) {
-    const parts = content.slice(4).split('\n---\n')
-    const prelude = parts[0]
-    content = parts.slice(1).join('\n---\n').trimStart()
-    Object.assign(opts, yaml.load(prelude))
+export async function build_md(path) {
+  let props = await read(path)
+  props = update(props, 'layout', 'document')
+  props = prelude(props)
+  if (props.typst) {
+    props = await typst(props, 16)
   }
-  opts.content = await marked.parse(content, {async: true})
-  const html = nunjucks.render(`${opts.layout}.njk`, opts)
-  await writeFile(`${info.name}.html`, html)
+  props = await md(props)
+  props = template(props)
+  await write(props)
 }
 
 export async function build(path) {
-  if(Array.isArray(path)) {
+  if (Array.isArray(path)) {
     const promises = []
-    for(const item of path) {
+    for (const item of path) {
       promises.push(build(item))
     }
     return Promise.all(promises)
@@ -101,34 +175,36 @@ export async function build(path) {
 
   const stats = await stat(path)
   const info = parse(path)
-  if(stats.isDirectory()) {
-    await build_dir(path)
-  }
-  else if(info.ext === '.md') {
+  if (stats.isDirectory()) {
+    await dir(path)
+  } else if (info.ext === '.md') {
     await build_md(path)
   }
 }
 
 export async function index(dirs, opts) {
-  opts = Object.assign({title: ''}, opts)
-  opts.links = await Promise.all(dirs.map(async dir => {
-    let title = dir
-    if((await readdir(dir)).includes('index.html')) {
-      const content = await readFile(join(dir, 'index.html'), {encoding: 'utf8'})
-      const re = new RegExp("<title>(.*)<\/title>")
-      const matches = content.match(re)
-      if(matches) {
-        title = matches[1]
-        
+  opts = Object.assign({ title: '' }, opts)
+  opts.links = await Promise.all(
+    dirs.map(async (dir) => {
+      let title = dir
+      if ((await readdir(dir)).includes('index.html')) {
+        const content = await readFile(join(dir, 'index.html'), {
+          encoding: 'utf8',
+        })
+        const re = new RegExp('<title>(.*)</title>')
+        const matches = content.match(re)
+        if (matches) {
+          title = matches[1]
+        }
       }
-    }
-    const link = relative(server_root, `./${dir}`)
-    return {
-      link: `/${link}/`,
-      title
-    }
-  }))
-  const content = nunjucks.render('index.njk', opts);
+      const link = relative(server_root, `./${dir}`)
+      return {
+        link: `/${link}/`,
+        title,
+      }
+    }),
+  )
+  const content = nunjucks.render('index.njk', opts)
   await writeFile('index.html', content)
 }
 
@@ -138,23 +214,30 @@ export async function build_and_index(dirs, opts) {
 }
 
 export async function redirect(target, opts) {
-  const link = target.startsWith('http') ? target : '/'+relative(server_root, `./${target}`)
+  const link = target.startsWith('http')
+    ? target
+    : '/' + relative(server_root, `./${target}`)
   opts.target = link
-  const content = nunjucks.render('redirect.njk', opts);
+  const content = nunjucks.render('redirect.njk', opts)
   await writeFile('index.html', content)
 }
 
-export async function screenshot_website(url, filename, width, height, actions) {
+export async function screenshot_website(
+  url,
+  filename,
+  width,
+  height,
+  actions,
+) {
   const browser = await puppeteer.launch({
-    headless: "new"
+    headless: 'new',
   })
   const page = await browser.newPage()
   await page.goto(url, {
     waitUntil: 'networkidle2',
   })
-  await page.setViewport({width, height})
-  if(actions)
-    await actions(page)
+  await page.setViewport({ width, height })
+  if (actions) await actions(page)
   await page.screenshot({
     path: filename,
   })
@@ -162,7 +245,7 @@ export async function screenshot_website(url, filename, width, height, actions) 
 }
 
 function filenamify(s) {
-  const re = new RegExp('[<>:"/\|?*]', 'g')
+  const re = new RegExp('[<>:"/|?*]', 'g')
   return s.replace(re, '_')
 }
 
@@ -175,13 +258,12 @@ export async function pdf(chapters, filename) {
     const port = srv.address().port
 
     const browser = await puppeteer.launch({
-      headless: "new"
+      headless: 'new',
     })
 
     const pdfDoc = await PDFDocument.create()
-  
-    for(const chapter of chapters) {
-      
+
+    for (const chapter of chapters) {
       const url = `http://localhost:${port}/${relative(server_root, chapter)}`
       const page = await browser.newPage()
       await page.goto(url, {
@@ -189,14 +271,14 @@ export async function pdf(chapters, filename) {
       })
       const bytes = await page.pdf({ format: 'a4', printBackground: true })
       const pdf = await PDFDocument.load(bytes)
-      for(let i=0; i<pdf.getPageCount(); i++) {
+      for (let i = 0; i < pdf.getPageCount(); i++) {
         const [page] = await pdfDoc.copyPages(pdf, [i])
         pdfDoc.addPage(page)
       }
     }
 
-    await writeFile(filename, await pdfDoc.save()) 
-  
+    await writeFile(filename, await pdfDoc.save())
+
     await browser.close()
 
     srv.close()
@@ -204,4 +286,17 @@ export async function pdf(chapters, filename) {
   })
 }
 
-export default { build, index, build_and_index, redirect }
+export default {
+  build,
+  index,
+  build_and_index,
+  redirect,
+  build_md,
+  read,
+  write,
+  md,
+  prelude,
+  template,
+  update,
+  dir,
+}
---
+++ build_lib/typst.mjs
@@ -0,0 +1,101 @@
+import { unlink, readFile, writeFile } from 'node:fs/promises'
+import util from 'node:util'
+import { exec as exec_legacy } from 'node:child_process'
+const exec = util.promisify(exec_legacy)
+
+async function base64_encode(file) {
+  var bitmap = await readFile(file)
+  return `data:image/svg+xml;base64,${bitmap.toString('base64')}`
+}
+
+function img_inline(src, font_size) {
+  return `<img class="typst-formula-inline" style="display: inline-block; margin: -${font_size / 2
+    }px 0;" src="${src}">`
+}
+
+function img_display(src, font_size) {
+  return `<img class="typst-formula-display" style="display: block; margin: 1em auto;" src="${src}">`
+}
+
+async function create_document_inline(filename, src, font_size) {
+  const content = `#set text(${font_size}pt)
+
+#set page(
+  width: auto,
+  height: auto,
+  margin: (x: 0pt, y: ${font_size / 2}pt),
+)
+
+$${src}$
+`
+  await writeFile(filename, content)
+}
+
+async function create_document_display(filename, src, font_size) {
+  const content = `#set text(${font_size}pt)
+
+#set page(
+  width: auto,
+  height: auto,
+  margin: 0pt,
+)
+
+$${src}$
+`
+  await writeFile(filename, content)
+}
+
+async function get_svg(filename) {
+  return await readfile(filename, { encoding: 'utf8' })
+}
+
+export async function compile_inline(src, font_size) {
+  await create_document_inline('tmp.typ', src, font_size)
+  // const proc = Bun.spawn(['typst', 'compile', 'tmp.typ', 'tmp.svg'], {
+  //   cwd: '.', // specify a working directory
+  // })
+  await exec('typst compile tmp.typ tmp.svg')
+  const img_src = await base64_encode('tmp.svg')
+  const img = img_inline(img_src, font_size)
+  await unlink('tmp.typ')
+  await unlink('tmp.svg')
+  return img
+}
+
+export async function compile_display(src, font_size) {
+  await create_document_display('tmp.typ', src, font_size)
+  // const proc = Bun.spawn(['typst', 'compile', 'tmp.typ', 'tmp.svg'], {
+  //   cwd: '.', // specify a working directory
+  // })
+  // await proc.exited
+  await exec('typst compile tmp.typ tmp.svg')
+  const img_src = await base64_encode('tmp.svg')
+  const img = img_display(img_src, font_size)
+  await unlink('tmp.typ')
+  await unlink('tmp.svg')
+  return img
+}
+
+export async function compile(src, font_size) {
+  const re = new RegExp('\\$\\$(.+?)\\$\\$')
+  const res = re.exec(src)
+  if (!res) return src
+  const start = src.substring(0, res.index)
+  const rest = src.substring(res.index + res[0].length)
+  const formula = res[1]
+  if (formula.startsWith(' ')) {
+    return (
+      start +
+      (await compile_display(formula, font_size)) +
+      (await compile(rest, font_size))
+    )
+  } else {
+    return (
+      start +
+      (await compile_inline(formula, font_size)) +
+      (await compile(rest, font_size))
+    )
+  }
+}
+
+export default compile
