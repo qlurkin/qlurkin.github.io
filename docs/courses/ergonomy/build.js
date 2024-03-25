c1a171f85562dca19c6a532aece4734f1f2c257c
Quentin Lurkin <qlurkin@gmail.com>
2024-03-25 14:34:53 +0100

    Continue Lab 3 Ergo

--- docs/courses/ergonomy/build.js
+++ docs/courses/ergonomy/build.js
@@ -1,5 +1,8 @@
 import { build_and_index } from 'build_lib'
 
-build_and_index(['lab1', 'lab2' /*, 'Course2', 'Course3', 'Course4'*/], {
-  title: 'User Interface Ergonomy',
-})
+build_and_index(
+  ['lab1', 'lab2', 'lab3' /*, 'Course2', 'Course3', 'Course4'*/],
+  {
+    title: 'User Interface Ergonomy',
+  },
+)
