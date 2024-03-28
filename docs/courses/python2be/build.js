eaece0d9b53a8dd778389aa7378ea99ce9b56817
Quentin Lurkin <qlurkin@gmail.com>
2024-03-28 09:05:33 +0100

    Edit 2BE Project

--- docs/courses/python2be/build.js
+++ docs/courses/python2be/build.js
@@ -1,17 +1,20 @@
-import { build_and_index } from "build_lib";
+import { build_and_index } from 'build_lib'
 
-build_and_index([
-  'course12',
-  'regex',
-  'course4',
-  'course5',
-  'ai',
-  'concurrency',
-  'lab1',
-  'lab2',
-  'lab3',
-  'lab4',
-  'project2023'
-], {
-  title: 'Python Part 3'
-})
+build_and_index(
+  [
+    'course12',
+    'regex',
+    'course4',
+    'course5',
+    'ai',
+    'concurrency',
+    'lab1',
+    'lab2',
+    'lab3',
+    'lab4',
+    'project24',
+  ],
+  {
+    title: 'Python Part 3',
+  },
+)
