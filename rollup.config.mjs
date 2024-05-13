f95c6f16bda296dc3be1afd2fe61a40a4e628738
Quentin Lurkin <qlurkin@gmail.com>
2024-05-13 14:55:55 +0200

    Build document.css

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import sass from 'rollup-plugin-sass'
+import * as core_sass from 'sass'
+import fs from 'fs'
+
+const result = core_sass.compile('scss/document.scss')
+fs.writeFileSync('docs/document.css', result.css)
+console.log('document.css DONE')
 
 export default [
   {
@@ -8,29 +14,29 @@ export default [
     output: {
       file: 'docs/deck.js',
       format: 'iife',
-      name: 'Deck'
+      name: 'Deck',
     },
-    plugins: [ 
+    plugins: [
       resolve(),
       commonjs(),
       sass({
-        insert: true
-      })
-    ]
+        insert: true,
+      }),
+    ],
   },
   {
     input: 'js/document.js',
     output: {
       file: 'docs/document.js',
       format: 'iife',
-      name: 'Doc'
+      name: 'Doc',
     },
-    plugins: [ 
+    plugins: [
       resolve(),
       commonjs(),
       sass({
-        insert: true
-      })
-    ]
-  }
+        insert: true,
+      }),
+    ],
+  },
 ]
