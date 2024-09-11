233c4d8feb9d5734a6b88c1499d11893d4dc8ca9
Quentin Lurkin <qlurkin@gmail.com>
2024-09-11 13:25:30 +0200

    Use pandoc

---
+++ docs/courses/scalable/cluster/index.md
@@ -0,0 +1,7 @@
+---
+title: "Kubernetes Cluster"
+---
+
+## tsh
+
+
--- docs/courses/scalable/docker/build.js
+++ docs/courses/scalable/docker/build.js
@@ -1,4 +1,4 @@
-import { build } from 'build_lib'
+import { pandoc } from 'build_lib'
 
 // redirect('https://docs.docker.com/get-started/', {title: 'Labo 1: Getting Started with Docker'})
-build('index.md')
+pandoc('index.md')
