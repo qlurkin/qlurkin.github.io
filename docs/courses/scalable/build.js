f41a368df654ed2531f407006b43985483b236b0
Quentin Lurkin <qlurkin@gmail.com>
2024-09-24 12:48:18 +0200

    Update index

--- docs/courses/scalable/build.js
+++ docs/courses/scalable/build.js
@@ -1,9 +1,13 @@
 import { build_and_index } from 'build_lib'
 
-build_and_index([
-  'docker',
-  'k8s',
-  //'project2023',
-  // 'intro',
-  // 'mandelbrot',
-], {title: 'Scalable Architecture'})
+build_and_index(
+  [
+    'docker',
+    'k8s',
+    'project2425',
+    //'project2023',
+    // 'intro',
+    // 'mandelbrot',
+  ],
+  { title: 'Scalable Architecture' },
+)
