8ed9ea773ef585dbafe0b9f55ba2d1958638d993
Quentin Lurkin <qlurkin@gmail.com>
2024-11-04 19:54:26 +0100

    Update index

--- docs/courses/python2ba/labs/build.js
+++ docs/courses/python2ba/labs/build.js
@@ -1,5 +1,5 @@
 import { build_and_index } from 'build_lib'
 
-build_and_index(['lab1', 'lab2', 'lab3', 'lab4'], {
+build_and_index(['lab1', 'lab2', 'lab3', 'lab4', 'lab5'], {
   title: 'Labos',
 })
