a9ad4fa42f25e32da6b2f29ac33d103893638b61
Quentin Lurkin <qlurkin@gmail.com>
2024-11-12 08:59:10 +0100

    Add Project

--- docs/courses/poo/build.js
+++ docs/courses/poo/build.js
@@ -1,5 +1,5 @@
 import { build_and_index } from 'build_lib'
 
-build_and_index(['slides', 'labs', 'project2023'], {
+build_and_index(['slides', 'labs', 'project24'], {
   title: 'Object Oriented Programming',
 })
