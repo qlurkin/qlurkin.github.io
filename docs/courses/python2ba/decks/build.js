f09600b92e7c9649e2589bb5476d6c67a7dedcee
Quentin Lurkin <qlurkin@gmail.com>
2024-12-11 11:58:15 +0100

    Add deck 7

--- docs/courses/python2ba/decks/build.js
+++ docs/courses/python2ba/decks/build.js
@@ -1,5 +1,8 @@
 import { build_and_index } from 'build_lib'
 
-build_and_index(['deck1', 'deck2', 'deck3', 'deck4', 'deck5', 'deck6'], {
-  title: 'Slides',
-})
+build_and_index(
+  ['deck1', 'deck2', 'deck3', 'deck4', 'deck5', 'deck6', 'deck7'],
+  {
+    title: 'Slides',
+  },
+)
---
+++ docs/courses/python2ba/decks/deck7/build.js
@@ -0,0 +1,3 @@
+import { redirect } from 'build_lib'
+
+redirect('index.pdf', { title: 'Cours 7 - Réseaux et Internet' })
