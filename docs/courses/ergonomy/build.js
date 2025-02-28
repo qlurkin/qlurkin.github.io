25d4547b95afde5f72b05faec0a2fdb807dd7809
Quentin Lurkin <qlurkin@gmail.com>
2025-02-28 09:06:23 +0100

    Updates

--- docs/courses/ergonomy/build.js
+++ docs/courses/ergonomy/build.js
@@ -1,7 +1,7 @@
 import { build_and_index } from 'build_lib'
 
 build_and_index(
-  ['lab1' /* 'lab2', 'lab3', 'lab4' , 'Course2', 'Course3'*/, 'deck1'],
+  ['lab1', 'lab2', 'lab3', 'deck1', 'deck2', 'deck3'],
   {
     title: 'User Interface Ergonomy',
   },
