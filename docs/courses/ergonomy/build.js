8dd9f59b6af024fd9bad3f6fb2caf80b1fc42dac
Quentin Lurkin <qlurkin@gmail.com>
2025-03-14 10:12:26 +0100

    Add Deck 4

--- docs/courses/ergonomy/build.js
+++ docs/courses/ergonomy/build.js
@@ -1,7 +1,7 @@
 import { build_and_index } from 'build_lib'
 
 build_and_index(
-  ['lab1', 'lab2', 'lab3', 'deck1', 'deck2', 'deck3'],
+  ['lab1', 'lab2', 'lab3', 'lab4', 'deck1', 'deck2', 'deck3', 'deck4'],
   {
     title: 'User Interface Ergonomy',
   },
