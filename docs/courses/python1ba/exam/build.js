cc20eaa51bc7efd74f27304621856aa7a36698f2
Quentin Lurkin <qlurkin@gmail.com>
2025-06-26 14:28:31 +0200

    Add may 25 exam

--- docs/courses/python1ba/exam/build.js
+++ docs/courses/python1ba/exam/build.js
@@ -1,5 +1,5 @@
 import { build_and_index } from 'build_lib'
 
-build_and_index(['example', 'janv24', 'mai24', 'sept24', 'janv25'], {
+build_and_index(['example', 'janv24', 'mai24', 'sept24', 'janv25', 'mai25'], {
   title: 'Examens précédents',
 })
