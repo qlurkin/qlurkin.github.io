7c7bea28fee5f840338bf70c3b4398641872c435
Quentin Lurkin <qlurkin@gmail.com>
2025-11-02 00:43:23 +0100

    Update

---
+++ docs/courses/signalprocessing/lab2/build.js
@@ -0,0 +1,20 @@
+import { pandoc, make_zip } from 'build_lib'
+import fs from "fs";
+
+const files = fs.readdirSync('.');
+
+const imgs = []
+
+for (const file of files) {
+  if(file.endsWith(".svg") || file.endsWith(".png")) {
+    imgs.push(file)
+  }
+}
+
+make_zip("lab2.zip", imgs.concat([
+    './expected_result_1.svg',
+    './2_1_sin_waves.ipynb',
+    './2_Digital Signal Processing.ipynb',
+]))
+
+pandoc('index.md')
