1edd593298cc45803f5ebdf6be132f038fff57cb
Quentin Lurkin <qlurkin@gmail.com>
2025-11-17 22:19:16 +0100

    Add presentation to lab2

--- docs/courses/signalprocessing/lab2/build.js
+++ docs/courses/signalprocessing/lab2/build.js
@@ -1,22 +1,27 @@
 import { pandoc, make_zip } from 'build_lib'
-import fs from "fs";
+import fs from 'fs'
 
-const files = fs.readdirSync('.');
+const files = fs.readdirSync('.')
 
 const imgs = []
 
 for (const file of files) {
-  if(file.endsWith(".svg") || file.endsWith(".png")) {
+  if (file.endsWith('.svg') || file.endsWith('.png')) {
     imgs.push(file)
   }
 }
 
-make_zip("lab2.zip", imgs.concat([
+make_zip(
+  'lab2.zip',
+  imgs.concat([
     './2_Digital Signal Processing.ipynb',
     './2_1_sin_waves.ipynb',
     './2_2_TL.ipynb',
     'data_DSP_exercise_1_v2.npz',
     'data_TL.npz',
-]))
+    './presentation.ipynb',
+  ]),
+)
 
 pandoc('index.md')
+
