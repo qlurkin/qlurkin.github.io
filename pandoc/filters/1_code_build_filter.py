0cb7adb208bf3bb1a2dbb1b7fe69638776f77b09
Quentin Lurkin <qlurkin@gmail.com>
2026-01-14 18:24:35 +0100

    Add python build

--- pandoc/filters/1_code_build_filter.py
+++ pandoc/filters/1_code_build_filter.py
@@ -3,6 +3,10 @@
 import re
 import subprocess as sp
 from base64 import b64encode
+import io
+import contextlib
+import os
+import sys
 
 import panflute as pf
 
@@ -31,12 +35,34 @@ def plantuml(elem, doc):
     return pf.RawBlock(html, format="html")
 
 
+def build_python(elem, doc):
+    src = elem.text
+    buffer = io.StringIO()
+    current_dir = os.getcwd()
+    dir_added = False
+    if current_dir not in sys.path:
+        dir_added = True
+        sys.path.insert(0, current_dir)
+
+    with contextlib.redirect_stdout(buffer):
+        exec(src)
+
+    if dir_added:
+        sys.path.remove(current_dir)
+
+    output = buffer.getvalue()
+
+    return pf.RawBlock(output, format="html")
+
+
 def action(elem, doc):
     if isinstance(elem, pf.CodeBlock):
         lang = elem.classes[0]
         if "build" in elem.classes:
             if lang == "plantuml":
                 return plantuml(elem, doc)
+            if lang == "python":
+                return build_python(elem, doc)
 
 
 def main(doc=None):
