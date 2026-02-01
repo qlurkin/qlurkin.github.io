6b7a7e7b6ebcfca7d537ab882cc5c06c52c5106c
Quentin Lurkin <qlurkin@gmail.com>
2026-02-01 15:29:34 +0100

    Hide old slides

--- docs/courses/python1ba/slides/build.js
+++ docs/courses/python1ba/slides/build.js
@@ -3,10 +3,10 @@ import { build_and_index } from 'build_lib'
 build_and_index(
   [
     'slides1',
-    'slides2',
-    'slides3',
-    'slides4',
-    //  'slides5',
+    // 'slides2',
+    // 'slides3',
+    // 'slides4',
+    // 'slides5',
   ],
   { title: 'Slides' },
 )
