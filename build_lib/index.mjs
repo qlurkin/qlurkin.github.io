cfd6f1d0339839107c6a31d85b2cba3015473494
Quentin Lurkin <qlurkin@gmail.com>
2026-03-31 02:05:15 +0200

    New Math Font

--- build_lib/index.mjs
+++ build_lib/index.mjs
@@ -169,7 +169,7 @@ export async function pandoc(path) {
       .map((file) => `--filter ${join(root, 'pandoc', 'filters', file)}`)
       .join(' ')
     const child = exec(
-      `pandoc -f markdown -t html -s --template ${join(root, 'pandoc', 'template.html')}  --section-divs -M document-css=false ${filter_args}  --mathjax`,
+      `pandoc -f markdown -t html -s --template ${join(root, 'pandoc', 'template.html')}  --section-divs -M document-css=false ${filter_args}  --mathml`,
       { maxBuffer: 10 * 1024 * 1024 },
       (error, stdout, stderr) => {
         if (stderr.length > 0) {
