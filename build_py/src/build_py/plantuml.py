1f974412064fccce734db906579ebd2a0635dcb2
Quentin Lurkin <qlurkin@gmail.com>
2026-06-08 14:59:18 +0200

    Add plantuml

---
+++ build_py/src/build_py/plantuml.py
@@ -0,0 +1,11 @@
+import subprocess as sp
+from pathlib import Path
+
+
+def plantuml(path: str | Path):
+    path = Path(path)
+    dest = path.parent / (path.stem + ".svg")
+    with open(dest, "w", encoding="utf8") as file:
+        cmd = ["plantuml", "--svg", str(path)]
+        sp.run(cmd, stdout=file)
+    print(f"PLANTUML {path.resolve()}")
