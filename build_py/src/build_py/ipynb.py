f1db1fe430dc6f15391ffaebd866a8e2017173de
Quentin Lurkin <qlurkin@gmail.com>
2026-06-08 14:59:11 +0200

    Add ipynb strip

---
+++ build_py/src/build_py/ipynb.py
@@ -0,0 +1,15 @@
+import json
+from pathlib import Path
+
+
+def ipynb(path: str | Path):
+    path = Path(path)
+    with open(path, encoding="utf8") as file:
+        nb = json.load(file)
+    for cell in nb["cells"]:
+        if cell["cell_type"] == "code":
+            cell["outputs"] = []
+            cell["execution_count"] = None
+    with open(path, "w", encoding="utf8") as file:
+        json.dump(nb, file, ensure_ascii=False, indent=2)
+    print(f"IPYNB {path.resolve()}")
